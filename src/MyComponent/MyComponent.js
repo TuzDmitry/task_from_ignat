4a3459671aa7f7518c1358245084452a0f222cd1
TuzDmitry <dimatuz94@gmail.com>
2020-03-12 23:45:44 +0300

    the first Task from Ignat/ created the Component: "MyComponent"

---
+++ src/MyComponent/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import my from './MyComponent.module.css'
+const MyComponent= ()=> {
+  return (
+    <div className={my.block}>
+        <div className={my.lastName}>Tuz Dmitry</div>
+        <div>домашка от меня №1: создать проект и выложить на github:
+            - создать компоненту, которая красиво будет выводить ваше ФИО
+            - *** создать компоненту, которая будет выводить текст,  стилизованно как в телеграмме (имя, текст, время, уголок)</div>
+        <input className={my.time} type="time" value="12:15" readOnly/>
+    </div>
+  );
+}
+
+export default MyComponent;
