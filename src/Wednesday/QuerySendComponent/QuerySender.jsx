e1bac9747b5e65304c0f799e95ed853980c5171b
TuzDmitry <dimatuz94@gmail.com>
2020-05-19 01:48:24 +0300

    the 11-th Task from Ignat/  finished//

--- src/Wednesday/QuerySendComponent/QuerySender.jsx
+++ src/Wednesday/QuerySendComponent/QuerySender.jsx
@@ -3,22 +3,17 @@ import React from "react";
 const QuerySender=(props)=>{
 
     const onCheckBoxClick=(e)=>{
-        let a=e.currentTarget.checked?1:0
-        // props.changeSuccess(a)
         props.changeSuccess(e.currentTarget.checked)
 
-        // alert(e.currentTarget.checked)
     }
     const onSendButtonClick=()=>{
-        props.funct()
+        // props.funct()
+        props.tryCatch()
     }
 
-
-debugger
 
     return(
         <div>
-            {/*<input type="checkbox" onChange={onCheckBoxClick} checked={props.success===1}/>*/}
             <input type="checkbox" onChange={onCheckBoxClick} checked={props.success}/>
             <button onClick={onSendButtonClick}>SEND</button>
         </div>
--- src/Wednesday/QuerySendComponent/QuerySenderContainer.js
+++ src/Wednesday/QuerySendComponent/QuerySenderContainer.js
@@ -10,63 +10,42 @@ class QuerySenderAPI extends React.Component {
 
     }
 
-    getQuery() {
-        debugger
-        axios.post('https://neko-cafe-back.herokuapp.com/auth/test',
-            {success: this.success}
-        ).then(response => {
-            debugger
-            console.log(response.data.yourBody)
-            alert(response.data.yourBody.success)
-        })
-            .catch(err => alert(err));
-    }
+    // getQuery() {
+    //     debugger
+    //     axios.post('https://neko-cafe-back.herokuapp.com/auth/test',
+    //         {success: this.success}
+    //     ).then(response => {
+    //         debugger
+    //         console.log(response.data.yourBody)
+    //         alert(response.data.yourBody.success)
+    //     })
+    //         .catch(err => alert(err));
+    // }
 
-    vvv = this.props.success
     tryCatch = async () => {
 
-        debugger
         try {
-
-            const responce = await axios.post('https://neko-cafe-back.herokuapp.com/auth/test',
+            const response = await axios.post('https://neko-cafe-back.herokuapp.com/auth/test',
                 {success: this.props.success}
             );
-            console.log('answer: ', responce.data.yourBody);
-            return responce.data.yourBody;
+            console.log('answer: ', response.data.yourBody.success);
+            return response.data.yourBody;
         } catch (e) {
-            // console.log('error: ', {...e});
-            console.log('error: ', {success: false});
+            console.log('error: ', {...e});
+            console.log('error: ', {...e.response.data.yourBody.success});
 
             return {success: false};
         }
     }
 
 
-    // axios.post(`https://social-network.samuraijs.com/api/1.1/todo-lists/${this.props.id}/tasks`,
-    //     {title: newText},
-    //     {
-    //         withCredentials: true,
-    //         headers: {"API-KEY": "99d1b1eb-87ca-41b0-b4eb-5da7df0ab7de"}
-    //     }
-    // ).then(response => {
-    //     if (response.data.resultCode === 0) {
-    //         let newTask = response.data.data.item;
-    //         this.props.addTask(this.props.id, newTask)
-    //     }
-    // })
-
-
     render() {
         return (
-            <>
-                <button onClick={() => {
-                    this.tryCatch()
-                }}>SEND2
-                </button>
-                <QuerySender success={this.props.success}
-                             changeSuccess={this.props.changeSuccess}
-                             funct={this.getQuery}/>
-            </>
+            <QuerySender success={this.props.success}
+                         changeSuccess={this.props.changeSuccess}
+                         tryCatch={this.tryCatch}
+                         funct={this.getQuery}/>
+
         )
     }
 }
